cc48d1e970f925b917e17ba01d46314cc0b24f20
ericoborgonove <borgonove@gmail.com>
2026-02-28 17:31:14 -0400

    doc: lib:hash.js

--- src/lib/hash.js
+++ src/lib/hash.js
@@ -1,1 +1,12 @@
-// file: src/lib/hash.js
+const bcrypt = require("bcryptjs");
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+}
+
+function comparePassword(password, hash) {
+  return bcrypt.compare(password, hash);
+}
+
+module.exports = { hashPassword, comparePassword };
